ba7ddc460f41e132a4fed7e6a513aae948fd6c32
hwisu <hwisu@users.noreply.github.com>
2026-08-10 13:47:14 +0900

    Prepare YKS 0.2.6 release

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "dev.yks"
-version = providers.gradleProperty("releaseVersion").getOrElse("0.2.4-SNAPSHOT")
+version = providers.gradleProperty("releaseVersion").getOrElse("0.2.6-SNAPSHOT")
 
 dependencyLocking {
     lockAllConfigurations()
